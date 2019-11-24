9406dbedb1b79327f99ca91a2cd13ee5ff630acb
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2019-11-24 12:06:39 +0530

    performance improvement by using prototypes

--- 12.ice/script.js
+++ 12.ice/script.js
@@ -3,105 +3,105 @@ const snowFlakes = [];
 let snowFlake;
 
 function setup() {
- createCanvas(window.innerWidth, window.innerHeight);
- fill(255);
- const initialX = random(width / 4,3*width/4);
- snowFlake = new SnowFlake(initialX);
+  createCanvas(window.innerWidth, window.innerHeight);
+  fill(255);
+  const initialX = random(width / 4, 3 * width / 4);
+  snowFlake = new SnowFlake(initialX);
 }
 
 function draw() {
- background(0);
- push();
- snowFlake.animate();
- pop();
- for(let i=snowFlakes.length-1;i>=0;i--) {
-  if(snowFlakes[i].translation > height*1.1) {
-   snowFlakes.splice(i,1);
+  background(0);
+  push();
+  snowFlake.animate();
+  pop();
+  for (let i = snowFlakes.length - 1; i >= 0; i--) {
+    if (snowFlakes[i].translation > height * 1.1) {
+      snowFlakes.splice(i, 1);
+    }
+    else {
+      push();
+      snowFlakes[i].animate();
+      pop();
+    }
   }
-  else {
-   push();
-   snowFlakes[i].animate();
-   pop();
+  if (snowFlake.translation > height / 3) {
+    snowFlakes.push(snowFlake);
+    const initialX = random(width / 4, 3 * width / 4);
+    snowFlake = new SnowFlake(initialX);
+    console.log(snowFlakes.length);
   }
- }
- if(snowFlake.translation > height/3) {
-  snowFlakes.push(snowFlake);
-  const initialX = random(width / 4,3*width/4);
-  snowFlake = new SnowFlake(initialX);
-  console.log(snowFlakes.length);
- }
 }
 
 function SnowFlake(initialX) {
- this.initialX = initialX;
- this.translation = 0;
- this.rotation = 0;
- this.ices = [];
- this.ice = new SnowParticle(this.initialX, 0,this.ices);
- this.count = 0;
- 
- this.animate = () => {
-  translate(this.initialX, this.translation+=2);
-  rotate(this.rotation+=PI/50);
+  this.initialX = initialX;
+  this.translation = 0;
+  this.rotation = 0;
+  this.ices = [];
+  this.ice = new SnowParticle(this.initialX, 0, this.ices);
+  this.count = 0;
+}
+
+SnowFlake.prototype.animate = function() {
+  translate(this.initialX, this.translation += 2);
+  rotate(this.rotation += PI / 50);
   scale(0.4);
   for (let i = 0; i < 6; i++) {
-   rotate(PI / 3);
-   push();
-   for (let i = 0; i < this.ices.length; i++) {
-    this.ices[i].draw();
-   }
+    rotate(PI / 3);
+    push();
+    for (let i = 0; i < this.ices.length; i++) {
+      this.ices[i].draw();
+    }
 
-   while (this.ice.shouldMove()) {
-    this.ice.update();
-   }
-   this.ice.draw();
+    while (this.ice.shouldMove()) {
+      this.ice.update();
+    }
+    this.ice.draw();
 
-   if (this.count < maxCount && Math.random()<0.2) {
-    this.ices.push(this.ice);
-    this.ice = new SnowParticle(this.initialX, random(-40,40),this.ices);
-    this.count++;
-   }
-   pop();
+    if (this.count < maxCount && Math.random() < 0.2) {
+      this.ices.push(this.ice);
+      this.ice = new SnowParticle(this.initialX, random(-40, 40), this.ices);
+      this.count++;
+    }
+    pop();
   }
- }
 }
 
 function SnowParticle(x, y, ices) {
- this.pos = createVector(x, y);
- this.radius = 5;
- this.ices = ices;
+  this.pos = createVector(x, y);
+  this.radius = 5;
+  this.ices = ices;
+}
 
- this.draw = () => {
-   ellipse(this.pos.x, this.pos.y, this.radius);
-   ellipse(this.pos.x, -this.pos.y, this.radius);
- };
+SnowParticle.prototype.draw = function () {
+  ellipse(this.pos.x, this.pos.y, this.radius);
+  ellipse(this.pos.x, -this.pos.y, this.radius);
+};
 
- this.update = () => {
+SnowParticle.prototype.update =  function() {
   this.pos.x += -1;
   this.pos.y += random(1, -1);
-  const angle = constrain(this.pos.heading(),0,PI/3);
+  const angle = constrain(this.pos.heading(), 0, PI / 3);
   const magnitude = this.pos.mag();
   this.pos.x = magnitude * Math.cos(angle);
   this.pos.y = magnitude * Math.sin(angle);
- };
+}
 
- this.shouldMove = () => {
-  if (this.pos.x < 1 || isColliding()) {
-   return false;
+SnowParticle.prototype.shouldMove = function() {
+  if (this.pos.x < 1 || this.isColliding()) {
+    return false;
   }
   return true;
- };
+}
 
- const isColliding = () => {
+SnowParticle.prototype.isColliding = function() {
   for (let currentIce of this.ices) {
-   const distance = dist(
-    currentIce.pos.x,
-    currentIce.pos.y,
-    this.pos.x,
-    this.pos.y
-   );
-   if (distance < this.radius) return true;
+    const distance = dist(
+      currentIce.pos.x,
+      currentIce.pos.y,
+      this.pos.x,
+      this.pos.y
+    );
+    if (distance < this.radius) return true;
   }
   return false;
- };
-}
+};
